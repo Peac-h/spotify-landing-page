162e6bf91782b84dcb64ab4c4e89f4b1baa25b92
_Peach <p6eac.h@gmail.com>
2024-05-26 13:22:54 +0400

    Fix redirecting

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import eSLint from "vite-plugin-eslint";
-import ViteSassPlugin from "vite-plugin-sass";
 
 export default defineConfig({
-  plugins: [react(), eSLint(), ViteSassPlugin()],
+  plugins: [react(), eSLint()],
 });
